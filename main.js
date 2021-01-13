bf94ef175b584e5ad2e1317b488cfbfd4962aee8
Tien Tang <48810730+tientang@users.noreply.github.com>
2021-01-13 15:44:20 -0800

    Update main.js

--- main.js
+++ main.js
@@ -7,8 +7,10 @@ ASSET_MANAGER.queueDownload("./dbz.png");
 ASSET_MANAGER.downloadAll(function () {
 	var canvas = document.getElementById('gameWorld');
 	var ctx = canvas.getContext('2d');
-
+	
+	
+	var goku = new Goku(gameEngine);
 	gameEngine.init(ctx);
-	gameEngine.addEntity(new Goku(this.gameEngine));
+	gameEngine.addEntity(goku);
 	gameEngine.start();
 });
