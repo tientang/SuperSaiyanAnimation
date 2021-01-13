d8868f2daff060277c70b3d7e125f80760a64393
Tien Tang <48810730+tientang@users.noreply.github.com>
2021-01-13 15:33:49 -0800

    Create goku.js

---
+++ goku.js
@@ -0,0 +1,19 @@
+class Goku {
+    constructor(game) {
+        Object.assign(this, {game});
+
+        this.spritesheet = ASSET_MANAGER.getAsset("./dbz.png");
+
+        this.animations = [];
+        
+        this.animations.push(new Animator(this.spritesheet, 40, 90, 28, 47, 11, 0.1, 7, false, true));
+    };
+
+    update() {
+
+    };
+
+    draw(ctx) {
+        this.animations[0].drawFrame(this.game.clockTick, ctx, 0, 0, 1);
+    };
+}
