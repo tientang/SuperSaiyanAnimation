8701d02c22014429a9a1363cd9524ae29c0fde43
Tien Tang <48810730+tientang@users.noreply.github.com>
2021-01-13 15:22:35 -0800

    Update goku.js
    removed loadAnimations method

--- goku.js
+++ goku.js
@@ -5,14 +5,8 @@ class Goku {
         this.spritesheet = ASSET_MANAGER.getAsset("./dbz.png");
 
         this.animations = [];
-        this.loadAnimations();
-    };
-
-    loadAnimations() {
-
-
-        this.animations[0] = new Animator(this.spritesheet, 40, 90, 28, 46, 11, 0.1, 7, false, true);
-        //this.animations[1] = new Animator(this.spritesheet, 40, 90, 28, 46, 11, 0.1, 7, false, true);
+        
+        this.animations.push(new Animator(this.spritesheet, 40, 90, 28, 47, 11, 0.1, 7, false, true));
     };
 
     update() {
