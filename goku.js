3feedadd36b1d6f13980cd5101e87196d1071224
Tien Tang <48810730+tientang@users.noreply.github.com>
2021-01-13 18:08:06 -0800

    Update goku.js

--- goku.js
+++ goku.js
@@ -8,7 +8,7 @@ class Goku {
         
                                                           // x, y, width, height, frameCount, frameDuration, spacing, reverse, loop
         //this.animations.push(new Animator(this.spritesheet, 60, 125, 45, 75, 6, 1, 3.25, false, true));
-        this.animations.push(new Animator(this.spritesheet, 57, 422, 48.875, 70, 5, 0.08825, 3.5, false, true));
+        this.animations.push(new Animator(this.spritesheet, 57, 422, 48.875, 75, 5, 0.08825, 3.5, false, true));
     };
 
     update() {
