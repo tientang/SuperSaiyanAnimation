de993fdf91130cdea25f907b67b906c8f7b569f9
Tien Tang <48810730+tientang@users.noreply.github.com>
2021-01-13 18:02:52 -0800

    Update goku.js

--- goku.js
+++ goku.js
@@ -8,7 +8,7 @@ class Goku {
         
                                                           // x, y, width, height, frameCount, frameDuration, spacing, reverse, loop
         //this.animations.push(new Animator(this.spritesheet, 60, 125, 45, 75, 6, 1, 3.25, false, true));
-        this.animations.push(new Animator(this.spritesheet, 57, 422, 48.875, 60, 5, 0.075, 3.5, false, true));
+        this.animations.push(new Animator(this.spritesheet, 57, 422, 48.875, 60, 5, 0.0875, 3.5, false, true));
     };
 
     update() {
@@ -16,6 +16,6 @@ class Goku {
     };
 
     draw(ctx) {
-        this.animations[0].drawFrame(this.game.clockTick, ctx, 175, 175, 7.5);
+        this.animations[0].drawFrame(this.game.clockTick, ctx, 185, 175, 6);
     };
 }
